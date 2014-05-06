043cac5cbf17393b52dccc6be6a7cb6f7e51d7b7
Dayana <dayana.sofia23@gmail.com>
2014-05-06 20:38:24 +0200

    Creado el archivo Users_roles Arreglados fallos.

--- src/Acme/MdmBundle/Entity/Roles_permissions.php
+++ src/Acme/MdmBundle/Entity/Roles_permissions.php
@@ -9,7 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity
  */
 
-class Groups{
+class Roles_permissions{
     
     private  $rpID;
     
---
+++ src/Acme/MdmBundle/Entity/Users_roles.php
@@ -0,0 +1,20 @@
+<?php
+namespace Acme\MdmBundle\Entity;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Acme\MdmBundle\Entity\Users_roles
+ *
+ * @ORM\Table(name="users_roles")
+ * @ORM\Entity
+ */
+
+class Users_roles{
+    
+    private  $urID;
+    
+    private $urrolID;
+    
+    private $urusrID;
+    
+}
