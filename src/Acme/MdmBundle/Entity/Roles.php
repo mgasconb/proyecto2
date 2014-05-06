26b5707c7df72b2d77df82808b38fc4a055e53bd
Dayana <dayana.sofia23@gmail.com>
2014-05-06 20:17:01 +0200

    Arreglados fallos con las mayusculas y minusculas en las clases.

---
+++ src/Acme/MdmBundle/Entity/Roles.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Acme\MdmBundle\Entity;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Acme\MdmBundle\Entity\Roles
+ *
+ * @ORM\Table(name="roles")
+ * @ORM\Entity
+ */
+
+class Tasks{
+    
+    private  $tarID;
+    
+    private $tarnombre;
+    
+    private $targrupoID;
+    
+    private $tarotros;
+   
+}
+
--- src/Acme/MdmBundle/Entity/Users_Tasks.php
+++ src/Acme/MdmBundle/Entity/Users_Tasks.php
@@ -4,13 +4,14 @@ namespace Acme\MdmBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * Acme\MdmBundle\Entity\Users_tasks
+ * Acme\MdmBundle\Entity\Users_Tasks
  *
- * @ORM\Table(name="Users_tasks")
+ * @ORM\Table(name="users_tasks")
  * @ORM\Entity
  */
 
 class Tasks{
+    private  $utID;
     
     private  $utusuarioID;
     
