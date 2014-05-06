e689859e1844e86438184132b737b3c3d034a823
Dayana <dayana.sofia23@gmail.com>
2014-05-06 20:10:25 +0200

    Creación del fichero Users_task

---
+++ src/Acme/MdmBundle/Entity/Users_Tasks.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Acme\MdmBundle\Entity;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Acme\MdmBundle\Entity\Users_tasks
+ *
+ * @ORM\Table(name="Users_tasks")
+ * @ORM\Entity
+ */
+
+class Tasks{
+    
+    private  $utusuarioID;
+    
+    private $uttareaID;
+    
+}
+
