638edc8fbe495b3ca1916b95169bf5e24248479c
Miguel <Miguel@192.168.1.60>
2014-05-09 22:59:36 +0200

    Cambios en el fichero user.php para especificar el tipo de datos de cada columna de acuerdo a la documentación de doctrine.

--- src/Acme/MdmBundle/Entity/User.php
+++ src/Acme/MdmBundle/Entity/User.php
@@ -12,18 +12,25 @@ use Doctrine\ORM\Mapping as ORM;
 
 class User{
     
+    /** @Column(type="integer") */
     private  $usrID;
     
+    /** @Column(length=45) */
     private $usrlogin;
     
+    /** @Column(length=45) */
     private $usrpassword;
     
+    /** @Column(length=45) */
     private $usrnombre;
     
+    /** @Column(length=45) */
     private $usrapellidos;
     
+    /** @Column(type="integer") */
     private $usrgrupoID;
     
+    /** @Column(length=45) */
     private $usrotros;
     
     
