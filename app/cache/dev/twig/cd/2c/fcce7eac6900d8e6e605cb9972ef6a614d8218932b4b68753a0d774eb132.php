8651bafbb194b06bb434837ea134f34ce7f8e1be
Dayana <dayana.sofia23@gmail.com>
2014-05-19 21:21:38 +0200

    Añadido CSS a la plantilla TWIG de registroUsuarios

---
+++ app/cache/dev/twig/cd/2c/fcce7eac6900d8e6e605cb9972ef6a614d8218932b4b68753a0d774eb132.php
@@ -0,0 +1,58 @@
+<?php
+
+/* TwigBundle:Exception:trace.txt.twig */
+class __TwigTemplate_cd2cfcce7eac6900d8e6e605cb9972ef6a614d8218932b4b68753a0d774eb132 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        if ($this->getAttribute((isset($context["trace"]) ? $context["trace"] : $this->getContext($context, "trace")), "function")) {
+            // line 2
+            echo "    at ";
+            echo twig_escape_filter($this->env, (($this->getAttribute((isset($context["trace"]) ? $context["trace"] : $this->getContext($context, "trace")), "class") . $this->getAttribute((isset($context["trace"]) ? $context["trace"] : $this->getContext($context, "trace")), "type")) . $this->getAttribute((isset($context["trace"]) ? $context["trace"] : $this->getContext($context, "trace")), "function")), "html", null, true);
+            echo "(";
+            echo twig_escape_filter($this->env, $this->env->getExtension('code')->formatArgsAsText($this->getAttribute((isset($context["trace"]) ? $context["trace"] : $this->getContext($context, "trace")), "args")), "html", null, true);
+            echo ")
+";
+        } else {
+            // line 4
+            echo "    at n/a
+";
+        }
+        // line 6
+        if (($this->getAttribute((isset($context["trace"]) ? $context["trace"] : null), "file", array(), "any", true, true) && $this->getAttribute((isset($context["trace"]) ? $context["trace"] : null), "line", array(), "any", true, true))) {
+            // line 7
+            echo "        in ";
+            echo twig_escape_filter($this->env, $this->getAttribute((isset($context["trace"]) ? $context["trace"] : $this->getContext($context, "trace")), "file"), "html", null, true);
+            echo " line ";
+            echo twig_escape_filter($this->env, $this->getAttribute((isset($context["trace"]) ? $context["trace"] : $this->getContext($context, "trace")), "line"), "html", null, true);
+            echo "
+";
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "TwigBundle:Exception:trace.txt.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  35 => 7,  29 => 4,  21 => 2,  51 => 15,  42 => 14,  38 => 13,  26 => 5,  24 => 4,  19 => 1,  57 => 16,  54 => 11,  43 => 8,  40 => 7,  33 => 6,  30 => 3,);
+    }
+}
