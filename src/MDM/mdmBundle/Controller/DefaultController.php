eca3bd2424d6470b61255951c8622eb21d6adb5e
Dayana <dayana.sofia23@gmail.com>
2014-05-22 10:18:11 +0200

    Nuevas funcionalidades:  funcioamiento del registro  del ususario

--- src/MDM/mdmBundle/Controller/DefaultController.php
+++ src/MDM/mdmBundle/Controller/DefaultController.php
@@ -5,7 +5,9 @@ namespace MDM\mdmBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use MDM\mdmBundle\Entity\Task;
 use MDM\mdmBundle\Entity\Users;
+use MDM\mdmBundle\Entity\Groups;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller {
 
@@ -31,41 +33,106 @@ class DefaultController extends Controller {
     }
 
     public function formUsersAction(Request $request) {
-        $user = new Users();
+        $id = 5;
+        $em = $this->getDoctrine()->getManager();
+        $group = $em->getRepository('mdmBundle:Groups')
+                ->find($id);
 
-        $form = $this->createFormBuilder($user)
+        /*  return new Response('Created product id ' .$group->getName());
+         */
+
+        $user = new Users();
+        $form = $this->createFormBuilder()
                 ->add('login', 'text')
                 ->add('password', 'text')
                 ->add('name', 'text')
                 ->add('surname', 'text')
                 ->add('other', 'text')
-                ->add('email', 'text')               
+                ->add('email', 'text')
                 ->add('save', 'submit')
                 ->getForm();
         /* ->add('group', 'text', array('mapped' => false))
-                ->add('id', 'text', array('mapped' => false))*/
-        
-        
-        
+          ->add('id', 'text', array('mapped' => false)) */
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+
+            $data = $form->getData();
+
+            $user->setName($data['name']);
+            $user->setEmail($data['email']);
+            $user->setSurname($data['surname']);
+            $user->setLogin($data['login']);
+            $user->setOther($data['other']);
+            $user->setPassword($data['password']);
+            $user->setGroup($group);
+
+
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+        }
+
         /*
           return $this->render('mdmBundle:Default:formularioUsuarios.html.twig', array(
           'form' => $form->createView(),
           ));
          */
 
-        
+
 
         /* Miramos si el formulario esta validado y recuperamos los datos. */
-        /*$form->handleRequest($request);*/
-        $form->bind( $request );
+        /* $form->handleRequest($request); */
+        /*    $form->bind( $request );
 
-        if ($form->isValid()) {
-            // perform some action, such as saving the task to the database
-            $nextAction = $form->get('save')->isClicked() ? 'mdm_homepage' : 'mdm_homepage';
-            return $this->redirect($this->generateUrl('mdm_form_task'));
-        }
-        
-        return $this->render('mdmBundle:Default:formularioUsuarios.html.twig', array('form' => $form->createView()));
+          if ($form->isValid()) {
+          // perform some action, such as saving the task to the database
+          $nextAction = $form->get('save')->isClicked() ? 'mdm_homepage' : 'mdm_homepage';
+          return $this->redirect($this->generateUrl('mdm_form_task'));
+          } */
+        /** Redirigir a otra pagina */
+        /*
+          return $this->render('mdmBundle:Default:formularioUsuarios.html.twig', array('form' => $form->createView()));
+         */
+        // una nueva pagina
+        // return $this->redirect($this->generateUrl('homepage'));
+        // un response con un bien venido 
+        return new Response('bienvenido');
+    }
+
+    function formloginAction(Request $request) {
+
+        $form = $this->createFormBuilder()
+                ->add('login', 'text')
+                ->add('password', 'text')
+                ->add('save', 'submit')
+                ->getForm();
+        /* ->add('group', 'text', array('mapped' => false))
+          ->add('id', 'text', array('mapped' => false)) */
+
+        $form->handleRequest($request);
+       
+         if ($form->isValid()) {
+            $data = $form->getData();
+            
+            $repository = $this->getDoctrine()
+                 ->getRepository('mdmBundle:Users');
+            $login->$repository->findOneBylogin($data['login']);
+            $password->$repository->findOneBypassword($data['password']);
+            
+            if(!$login && !$password ){
+                return new Response('No existe'); 
+            }
+            else{ return new Response('bienvenido');
+            
+            }
+            
+         }
+         return $this->render('mdmBundle:Default:formularioGrupo.html.twig', array(
+                    'form' => $form->createView(),
+        ));
     }
 
 }
